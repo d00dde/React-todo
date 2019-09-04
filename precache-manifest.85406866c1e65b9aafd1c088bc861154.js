355a2ce650a982e47e43bf73a61c6d1bc7508abc
Andrey <d48564@gmail.com>
2019-09-04 19:54:08 +0300

    Initial commit

---
+++ precache-manifest.85406866c1e65b9aafd1c088bc861154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a316a61e0e17a21b7fb52632980367",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "9e7d074011b9b77e2756",
+    "url": "/React-todo/static/css/main.3e7f7fc3.chunk.css"
+  },
+  {
+    "revision": "66f395d302fbe0e167c4",
+    "url": "/React-todo/static/js/2.b6fe4141.chunk.js"
+  },
+  {
+    "revision": "9e7d074011b9b77e2756",
+    "url": "/React-todo/static/js/main.e991c0a7.chunk.js"
+  },
+  {
+    "revision": "45aea7dcebb6519058d1",
+    "url": "/React-todo/static/js/runtime~main.d5f1644e.js"
+  }
+]);
